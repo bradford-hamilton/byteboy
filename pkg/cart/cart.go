c95c273dbef76ad5438fe93904fdc99db95bf4f8
bradford lamson-scribner <brad.lamson@gmail.com>
2024-06-27 05:24:02 -0600

    add write and read byte methods for the mmu

--- pkg/cart/cart.go
+++ pkg/cart/cart.go
@@ -1,5 +1,13 @@
 package cart
 
+// MBCs
+// As the Game Boy’s 16-bit address bus offers only limited space for ROM and RAM
+// addressing, many games are using Memory Bank Controllers (MBCs) to expand the
+// available address space by bank switching. These MBC chips are located in the
+// game cartridge (that is, not in the Game Boy itself). In each cartridge, the
+// required (or preferred) MBC type should be specified in the byte at $0147 of
+// the ROM, as described in the cartridge header.
+
 type Cart struct{}
 
 func New() *Cart { return &Cart{} }
--- pkg/mem/mem.go
+++ pkg/mem/mem.go
@@ -1,5 +1,7 @@
 package mem
 
+import "fmt"
+
 // MMU represents the GameBoy's memory management unit.
 type MMU struct {
 	// 32 KB ROM total (00-7FFF)
@@ -32,4 +34,74 @@ type MMU struct {
 	// ioReg [0x80]byte
 }
 
-func New() *MMU { return &MMU{} }
+func New() *MMU {
+	return &MMU{
+		rom:  [32768]byte{},
+		vram: [8192]byte{},
+		eram: [8192]byte{},
+		wram: [8192]byte{},
+		oam:  [160]byte{},
+		hram: [128]byte{},
+		ie:   0,
+	}
+}
+
+// ReadByte reads a byte from the given address.
+func (m *MMU) ReadByte(addr uint16) byte {
+	switch {
+	case addr <= 0x3FFF:
+		return m.rom[addr]
+	case addr <= 0x7FFF:
+		return m.rom[addr]
+	case addr <= 0x9FFF:
+		return m.vram[addr-0x8000]
+	case addr <= 0xBFFF:
+		return m.eram[addr-0xA000]
+	case addr <= 0xDFFF:
+		return m.wram[addr-0xC000]
+	case addr <= 0xFDFF:
+		return m.wram[addr-0xE000]
+	case addr <= 0xFE9F:
+		return m.oam[addr-0xFE00]
+	case addr <= 0xFEFF:
+		// Not usable
+		fmt.Println("TODO")
+		return 0xFF
+	case addr <= 0xFF7F:
+		// TODO: I/O registers
+		return 0xFF
+	case addr <= 0xFFFE:
+		return m.hram[addr-0xFF80]
+	case addr == 0xFFFF:
+		return m.ie
+	default:
+		return 0xFF
+	}
+}
+
+// WriteByte writes a byte to the given address.
+func (m *MMU) WriteByte(addr uint16, value byte) {
+	switch {
+	case addr <= 0x7FFF:
+		// ROM is typically not writable, but handle as needed for mappers.
+	case addr <= 0x9FFF:
+		m.vram[addr-0x8000] = value
+	case addr <= 0xBFFF:
+		m.eram[addr-0xA000] = value
+	case addr <= 0xDFFF:
+		m.wram[addr-0xC000] = value
+	case addr <= 0xFDFF:
+		m.wram[addr-0xE000] = value
+	case addr <= 0xFE9F:
+		m.oam[addr-0xFE00] = value
+	case addr <= 0xFEFF:
+		// Not usable
+		fmt.Println("TODO")
+	case addr <= 0xFF7F:
+		// TODO: I/O registers
+	case addr <= 0xFFFE:
+		m.hram[addr-0xFF80] = value
+	case addr == 0xFFFF:
+		m.ie = value
+	}
+}
